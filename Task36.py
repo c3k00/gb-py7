a4ef6063b56755fc2e84cfac01d0361dc696a92c
Artsiom <c3k00@proton.me>
2023-08-06 19:19:24 +0300

    task

--- Task36.py
+++ Task36.py
@@ -15,3 +15,12 @@
 # 4 8 12 16 20 24
 # 5 10 15 20 25 30
 # 6 12 18 24 30 36
+
+
+def print_operation_table(operation, num_rows, num_columns):
+  for i in range(1, num_rows + 1):
+    for j in range(1, num_columns + 1):
+      print(operation(i, j), end='\t')
+    print()
+
+print_operation_table(lambda x, y: x * y, int(input('row: ')), int(input('column: '))) 
